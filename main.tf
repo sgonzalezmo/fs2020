a7594b6085d9d0e3f3c0f7b86540a16930c1ce21
Samuel Gonzalez Morro <samuel.gonzalez.m@itnow.es>
2020-12-09 11:14:17 +0100

    Undo previous changes

--- main.tf
+++ main.tf
@@ -1,83 +1,83 @@
 data "ibm_is_ssh_key" "sshkey1" {
-  name = var.ssh_key_name
+  name = "${var.ssh_key_name}"
 }
 
 resource "ibm_is_vpc" "vpc1" {
-  name = var.vpc_name
-  address_prefix_management = "auto"
+  name = "${var.vpc_name}"
+  address_prefix_management = "manual"
 }
 
 resource "ibm_is_vpc_address_prefix" "vpc-ap1" {
   name = "vpc-ap1"
-  zone = var.zone1
-  vpc  = ibm_is_vpc.vpc1.id
-  cidr = var.zone1_cidr
+  zone = "${var.zone1}"
+  vpc  = "${ibm_is_vpc.vpc1.id}"
+  cidr = "${var.zone1_cidr}"
 }
 
 resource "ibm_is_vpc_address_prefix" "vpc-ap2" {
   name = "vpc-ap2"
-  zone = var.zone2
-  vpc  = ibm_is_vpc.vpc1.id
-  cidr = var.zone2_cidr
+  zone = "${var.zone2}"
+  vpc  = "${ibm_is_vpc.vpc1.id}"
+  cidr = "${var.zone2_cidr}"
 }
 
 resource "ibm_is_subnet" "subnet1" {
   name            = "subnet1"
-  vpc             = ibm_is_vpc.vpc1.id
-  zone            = var.zone1
-  ipv4_cidr_block = var.zone1_cidr
-  depends_on      = [ ibm_is_vpc_address_prefix.vpc-ap1 ]
+  vpc             = "${ibm_is_vpc.vpc1.id}"
+  zone            = "${var.zone1}"
+  ipv4_cidr_block = "${var.zone1_cidr}"
+  depends_on      = ["ibm_is_vpc_address_prefix.vpc-ap1"]
 }
 
 resource "ibm_is_subnet" "subnet2" {
   name            = "subnet2"
-  vpc             = ibm_is_vpc.vpc1.id
-  zone            = var.zone2
-  ipv4_cidr_block = var.zone2_cidr
-  depends_on      = [ ibm_is_vpc_address_prefix.vpc-ap2 ]
+  vpc             = "${ibm_is_vpc.vpc1.id}"
+  zone            = "${var.zone2}"
+  ipv4_cidr_block = "${var.zone2_cidr}"
+  depends_on      = ["ibm_is_vpc_address_prefix.vpc-ap2"]
 }
 
 resource "ibm_is_instance" "instance1" {
   name    = "instance1"
-  image   = var.image
-  profile = var.profile
+  image   = "${var.image}"
+  profile = "${var.profile}"
 
   primary_network_interface = {
-    subnet = ibm_is_subnet.subnet1.id
+    subnet = "${ibm_is_subnet.subnet1.id}"
   }
-  vpc  = ibm_is_vpc.vpc1.id
-  zone = var.zone1
-  keys = [ data.ibm_is_ssh_key.sshkey1.id ]
-  user_data = data.template_cloudinit_config.cloud-init-apptier.rendered
+  vpc  = "${ibm_is_vpc.vpc1.id}"
+  zone = "${var.zone1}"
+  keys = ["${data.ibm_is_ssh_key.sshkey1.id}"]
+  user_data = "${data.template_cloudinit_config.cloud-init-apptier.rendered}"
 }
 
 resource "ibm_is_instance" "instance2" {
   name    = "instance2"
-  image   = var.image
-  profile = var.profile
+  image   = "${var.image}"
+  profile = "${var.profile}"
 
   primary_network_interface = {
-    subnet = ibm_is_subnet.subnet2.id
+    subnet = "${ibm_is_subnet.subnet2.id}"
   }
-  vpc  = ibm_is_vpc.vpc1.id
-  zone = var.zone2
-  keys = [ data.ibm_is_ssh_key.sshkey1.id ]
-  user_data = data.template_cloudinit_config.cloud-init-apptier.rendered
+  vpc  = "${ibm_is_vpc.vpc1.id}"
+  zone = "${var.zone2}"
+  keys = ["${data.ibm_is_ssh_key.sshkey1.id}"]
+  user_data = "${data.template_cloudinit_config.cloud-init-apptier.rendered}"
 }
 
 resource "ibm_is_floating_ip" "floatingip1" {
   name = "fip1"
-  target = ibm_is_instance.instance1.primary_network_interface.0.id
+  target = "${ibm_is_instance.instance1.primary_network_interface.0.id}"
 }
 
 resource "ibm_is_floating_ip" "floatingip2" {
   name = "fip2"
-  target = ibm_is_instance.instance2.primary_network_interface.0.id
+  target = "${ibm_is_instance.instance2.primary_network_interface.0.id}"
 }
 
 resource "ibm_is_security_group_rule" "sg1_tcp_rule_22" {
-  depends_on = [ ibm_is_floating_ip.floatingip1, ibm_is_floating_ip.floatingip2 ]
-  group     = ibm_is_vpc.vpc1.default_security_group
+  depends_on = ["ibm_is_floating_ip.floatingip1", "ibm_is_floating_ip.floatingip2"]
+  group     = "${ibm_is_vpc.vpc1.default_security_group}"
   direction = "inbound"
   remote    = "0.0.0.0/0"
   tcp = {
@@ -87,8 +87,8 @@ resource "ibm_is_security_group_rule" "sg1_tcp_rule_22" {
 }
 
 resource "ibm_is_security_group_rule" "sg1_tcp_rule_80" {
-  depends_on = [ ibm_is_floating_ip.floatingip1, ibm_is_floating_ip.floatingip2]
-  group     = ibm_is_vpc.vpc1.default_security_group
+  depends_on = ["ibm_is_floating_ip.floatingip1", "ibm_is_floating_ip.floatingip2"]
+  group     = "${ibm_is_vpc.vpc1.default_security_group}"
   direction = "inbound"
   remote    = "0.0.0.0/0"
   tcp = {
