445d7fc7bbdf7d3dadd069270187112114034e92
Samuel Gonzalez Morro <samuel.gonzalez.m@itnow.es>
2020-12-09 10:46:06 +0100

    Change provider information.

--- provider.tf
+++ provider.tf
@@ -1,4 +1,9 @@
-provider "ibm" {
-  generation            = 2
-  region                = "${var.ibmcloud_region}"
+terraform {
+  required_providers {
+    ibm = {
+      source = "IBM-Cloud/ibm"
+      version = "~> 1.16.0"
+      region                = var.ibmcloud_region
+    }
+  }
 }
