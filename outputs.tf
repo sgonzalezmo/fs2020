a33dc363d3965ff34901f40e7da31fc63351a7b8
Samuel Gonzalez Morro <sgonzalezmo@gmail.com>
2020-12-14 12:46:03 +0100

    Fix Warning: Interpolation-only expressions are deprecated

--- outputs.tf
+++ outputs.tf
@@ -1,9 +1,9 @@
 output "FloatingIP-1" {
-    value = "${ibm_is_floating_ip.floatingip1.address}"
+  value = ibm_is_floating_ip.floatingip1.address
 }
 output "FloatingIP-2" {
-    value = "${ibm_is_floating_ip.floatingip2.address}"
+  value = ibm_is_floating_ip.floatingip2.address
 }
 output "LB-Hostname" {
-    value = "http://${ibm_is_lb.lb1.hostname}"
+  value = "http://${ibm_is_lb.lb1.hostname}"
 }
